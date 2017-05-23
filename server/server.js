cb3b89ccc70e63be6a9ba3ad965e4d82ce2c4c4c
aaqilcs102 <aaqilcs102@gmail.com>
2017-05-23 12:50:40 +0500

    another Update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 var app = express();
 const address = path.join(__dirname, '../public');
-const port = proccess.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(address));
 
 
